33886e056241c7807bdee913198c5615c5e3cd87
abutahar <mdabutaharmondol@gmail.com>
2023-09-30 01:31:22 +0600

    project setup for fably backend

---
+++ index.js
@@ -0,0 +1,18 @@
+// dependencies
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+env.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
